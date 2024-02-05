fb6d41835bb0f4a5da36dec75c2bf0464b50824e
janthony25 <junanthony.business@gmail.com>
2024-02-06 08:30:34 +1300

    Add project files.

---
+++ OOP-Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // generic = "not specific to a particular data type"
+            //            add <T> to: classes, methods, fields, etc
+            //            allows for code reusability for different data types
+
+            int[] intArray = { 1, 2, 3 };
+            double[] doubleArray = { 1.0, 2.0, 3.0 };
+            string[] stringArray = { "1", "2", "3" };
+
+            displayElemts(intArray);
+
+            Console.ReadKey();
+        }
+
+        public static void displayElemts<T>(T[] array)
+        {
+            foreach(T item in array)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
